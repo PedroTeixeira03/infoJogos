c6dfa3c3f4fdc85f4aed841e380fae84268cec85
sofsv <spdsvv@gmail.com>
2025-04-16 19:47:36 +0100

    Correções ficheiro que gere os formulários
    Verificação de qual o formulário foi ativado.
    Verificação dos campos.
    Envio do pedido ao back-end

--- registar.php
+++ registar.php
@@ -2,54 +2,39 @@
 include('ligaDados.php'); 
 $db = new ligaDados();
 
-if( isset($_POST['user']) && isset($_POST['email']) && isset($_POST['pass1']) && isset($_POST['pass2'])) {
-  if($_POST['pass1'] == $_POST['pass2']){
-    $password1 =MD5($_POST["passe1"]);
-		$password2 =MD5($_POST["passe2"]);
-    
-    $db->registo($_POST['user'],$_POST['email'],$password1,$password2);
-
+//Registar Utilizador
+if (isset($_POST['registar'])){
+  if(isset($_POST['user']) && isset($_POST['email']) && isset($_POST['pass1']) && isset($_POST['pass2'])) {
+    if($_POST['pass1'] == $_POST['pass2']){
+      $password1 =MD5($_POST["pass1"]);
+    $password2 =MD5($_POST["pass2"]);
+      
+      $db->registo($_POST['user'],$_POST['email'],$password1,$password2);
+    }
+    else{
+      echo("Password não coincide");
+    }
   }
-  else{
-    echo("Password não coincide");
-  }
-
 }
 
+//Efetuar o login
+if(isset($_POST['login'])){
+  if(isset($_POST['usermail']) && isset($_POST['pwd'])) {
+    echo "reg: ".$_POST['usermail']."e pass ".$_POST['pwd'];
+    if(!empty($_POST['pwd']) && !empty($_POST['usermail'])){
+      $password1 =MD5($_POST["pwd"]);
+      $db->login($_POST['usermail'],$password1);
+    }
+    else{
+      echo("Preencha todos os campos");
+    }
+  }
+}
 
-					if(!empty($_POST['utilizador'])) {						
-						$utilizador = $_POST["login"];
-            $email = $_POST["email"];
-						$password1 =MD5($_POST["passe1"]);
-						$password2 =MD5($_POST["passe2"]);
-						if($password1==$password2)
-						{					
-							$sql = "INSERT INTO utilizadores VALUES(null, '$utilizador', '$email','$password1')";
-							$resultado =mysqli_query($ligacao, $sql);
-
-							if ($resultado) {
-								echo "Dados inseridos com sucesso <br>";
-               $_SESSION['login'] = true;
-              $_SESSION['loginMsg'] = "<p align='center' style='color: blue;'>Login com sucesso </p>  "; 
-              $_SESSION['nome']=$row['nome'];
-          
-              mysqli_free_result($result);
-             //header('Location: index.php');
-
-							} else {
-								echo "Erro: " . $sql . "<br>" . mysqli_error($conn);
-                $_SESSION['loginMsg'] = "<br><br><p align='center' style='float: right; color: red;'>Login Falhou </p>  ";   
-              $_SESSION['tipo']=0;   // Utilizador tipo não autenticado
-              $_SESSION['nome']="erro";
-             // header('Location: index.php');
-							}
-						} else {
-              
-              echo "Palavras passe diferentes";
-            }
-            mysqli_close($ligacao);
-          
-          }
-		
+//Efetuar o logout
+if (isset($_GET['l']) && $_GET['l']== 0){
+  echo("logout");
+  $db->logout();
+}
 
 ?>
